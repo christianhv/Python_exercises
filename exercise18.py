e760f3ab4608dbf1ea3ee40e6c9c08e1a3b084bb
christianhv <christian.hv@me.com>
2015-02-18 11:21:42 +0100

    new

--- exercise18.py
+++ exercise18.py
@@ -6,4 +6,26 @@ def print_two(*args):
     arg1, arg2 = args
     print "arg1 = %r, arg2 = %r" % (arg1,arg2)
     
-#Ok, that *args is actually pointless, we can
+#Ok, that *args is actually pointless, we can do just this
+def print_two_again(arg1, arg2):
+    print "arg1 = %r, arg2 = %r" % (arg1,arg2)
+    
+#This just print one argument
+def print_one(arg1):
+    print "arg1 = %r" % arg1
+    
+#this one takes no arguments
+def print_none():
+    print "I got nothing"
+    
+    
+def main():
+    print_two(5,"hola")
+    print_two_again("bien", "eh")
+    print_one("Pos nomas uno")
+    print_none()
+    
+
+main()
+    
+    
